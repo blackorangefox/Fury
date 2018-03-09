4e7a655a2adc9e8b86b6b322af736ef1400e3ff8
Mikhail Fokin <mikhail.fokin.v@gmail.com>
2018-03-09 10:36:23 +0300

    fixed PreStartController

--- Service/PlayerService.swift
+++ Service/PlayerService.swift
@@ -7,66 +7,23 @@
 //
 
 import Foundation
-import AVFoundation
-import MediaPlayer
+import SwiftySound
 
 class PlayerService {
     
-    var startWork : AVAudioPlayer
-    var oneSecondPlayer: AVAudioPlayer
-    var startOrFinisRoundPlayer: AVAudioPlayer
-    
     init() {
-        let audioSession = AVAudioSession.sharedInstance()
-        try! audioSession.setCategory(AVAudioSessionCategoryPlayback, with: [AVAudioSessionCategoryOptions.mixWithOthers]) //Causes audio from other sessions to be ducked (reduced in volume) while audio from this session plays
-        try! audioSession.setActive(true)
-        
-        let path_1 = Bundle.main.path(forResource: "Airhorn", ofType: "mp3")
-        let filePath_1 = NSURL(fileURLWithPath: path_1!)
-        startWork = try! AVAudioPlayer.init(contentsOf: filePath_1 as URL)
-        startWork.volume = 1.0
-        startWork.numberOfLoops = 0
-        startWork.prepareToPlay()
-        
-        let path_2 = Bundle.main.path(forResource: "long-beep", ofType: "mp3")
-        let filePath_2 = NSURL(fileURLWithPath: path_2!)
-        startOrFinisRoundPlayer = try! AVAudioPlayer.init(contentsOf: filePath_2 as URL)
-        startOrFinisRoundPlayer.volume = 1.0
-        startOrFinisRoundPlayer.numberOfLoops = 0
-        startOrFinisRoundPlayer.prepareToPlay()
-        
-        let path_3 = Bundle.main.path(forResource: "1_second", ofType: "mp3")
-        let filePath_3 = NSURL(fileURLWithPath: path_3!)
-        oneSecondPlayer = try! AVAudioPlayer.init(contentsOf: filePath_3 as URL)
-        oneSecondPlayer.volume = 1.0
-        oneSecondPlayer.numberOfLoops = 0
-        oneSecondPlayer.prepareToPlay()
+        Sound.category = .ambient
     }
     
-    
     func playStartWork() {
-        startWork.play()
+        Sound.play(file: "Airhorn.mp3")
     }
     
     func playStartOrFinisRound() {
-        startOrFinisRoundPlayer.play()
+        Sound.play(file: "long-beep.mp3")
     }
     
     func playOneSecond() {
-        oneSecondPlayer.play()
+        Sound.play(file: "1_second.mp3")
     }
-    
-//    private func playSound(name: String, loop: Int) {
-//        //(MPVolumeView().subviews.filter{NSStringFromClass($0.classForCoder) == "MPVolumeSlider"}.first as? UISlider)?.setValue(1, animated: false)
-//        if let path = Bundle.main.path(forResource: name, ofType: "mp3") {
-//            let filePath = NSURL(fileURLWithPath: path)
-//            songPlayer = try! AVAudioPlayer.init(contentsOf: filePath as URL)
-//            songPlayer?.volume = 1.0
-//            songPlayer?.numberOfLoops = loop
-//            songPlayer?.prepareToPlay()
-//            songPlayer?.play()
-//        }
-//    }
-    
-    
 }
